4051aea47d987a9f2eee328505e875a49f3d630c
sr1jan <srofficialsingh@gmail.com>
2022-11-09 11:40:52 +0530

    add gitignore, remove dist & node_modules dir

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./rep3-whitelistor.cjs.production.min.js')
-} else {
-  module.exports = require('./rep3-whitelistor.cjs.development.js')
-}
