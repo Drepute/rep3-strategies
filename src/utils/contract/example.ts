ab78e1349c5ecaa83b6d73a493abcc0446a26e05
Jashan Shetty <43118727+eth-jashan@users.noreply.github.com>
2022-11-28 23:41:39 +0530

    example cleanup

--- src/utils/contract/example.ts
+++ src/utils/contract/example.ts
@@ -1,7 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 
-import ActionCaller from '../../actions';
-import { ActionOnType } from '../../actions/utils/type';
 import ContractCaller from './index';
 import { abi } from './utils/abis/erc20Abi';
 (async () => {
@@ -22,17 +20,6 @@ import { abi } from './utils/abis/erc20Abi';
       '0x565CBd65Cb3e65445AfD14169003A528C985e9C7'
     );
     console.log('result', result);
-    if (result.toString() >= 0) {
-      const actions = new ActionCaller(
-        '0xb24836d0f1441d69f4ddf0fa48de907a5751c892',
-        ActionOnType.membership,
-        '0x565CBd65Cb3e65445AfD14169003A528C985e9C7',
-        137,
-        { changingLevel: 2 }
-      );
-      const final = await actions.calculateActionParams();
-      console.log('results', final);
-    }
     console.timeEnd('contract Call');
   } catch (error) {
     console.error(error);
