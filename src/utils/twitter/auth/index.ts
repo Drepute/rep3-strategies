827309ad54d3a8adbe8bb6fcf7e3896319b1aa0f
Jashan Shetty <43118727+eth-jashan@users.noreply.github.com>
2022-12-05 17:39:01 +0530

    fix(files structure): removed Twitter Files
    Files containing twitter whitelister has been removed

--- src/utils/twitter/auth/index.ts
+++
@@ -1,9 +0,0 @@
-import { OAuth2User } from 'twitter-api-sdk/dist/OAuth2User';
-
-const getOauthURL = (authClient: OAuth2User, state: string) => {
-  const authUrl = authClient.generateAuthURL({
-    state,
-    code_challenge_method: 's256',
-  });
-  return authUrl;
-};
