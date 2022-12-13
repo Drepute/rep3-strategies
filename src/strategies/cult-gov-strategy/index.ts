52758cd38aece3db3069eb9187d77261079a2f2a
Jashan Shetty <43118727+eth-jashan@users.noreply.github.com>
2022-12-14 03:30:39 +0530

    Cult Gov Strategy

--- src/strategies/cult-gov-strategy/index.ts
+++ src/strategies/cult-gov-strategy/index.ts
@@ -3,6 +3,66 @@ import { ActionOnType } from '../../actions/utils/type';
 import { StrategyParamsType } from '../../types';
 import { subgraph } from '../../utils';
 
+const initializeActionClass = (
+  contractAddress: string,
+  eoa: string,
+  level: number
+) => {
+  return new ActionCaller(contractAddress, ActionOnType.membership, eoa, 1, {
+    changingLevel: level,
+  });
+};
+
+// lvl-1: = 0 proposal votes
+// lvl-2: = 1 proposal votes
+// lvl-3: = 3 proposal votes
+// lvl-4: = 5 proposal votes
+// lvl-5: = 7 proposal votes
+// lvl-6: = 9 proposal votes
+// lvl-7: = 11 proposal votes
+// lvl-8: >= 13 proposal votes
+
+const calculateActionParams = async (
+  proposals: number,
+  contractAddress: string,
+  eoa: string
+) => {
+  switch (proposals >= 0) {
+    case proposals === 0: {
+      const action = initializeActionClass(contractAddress, eoa, 1);
+      return await action.calculateActionParams();
+    }
+    case proposals <= 2: {
+      const action = initializeActionClass(contractAddress, eoa, 2);
+      return await action.calculateActionParams();
+    }
+    case proposals <= 4: {
+      const action = initializeActionClass(contractAddress, eoa, 3);
+      return await action.calculateActionParams();
+    }
+    case proposals <= 6: {
+      const action = initializeActionClass(contractAddress, eoa, 4);
+      return await action.calculateActionParams();
+    }
+    case proposals <= 8: {
+      const action = initializeActionClass(contractAddress, eoa, 5);
+      return await action.calculateActionParams();
+    }
+    case proposals <= 10: {
+      const action = initializeActionClass(contractAddress, eoa, 6);
+      return await action.calculateActionParams();
+    }
+    case proposals <= 12: {
+      const action = initializeActionClass(contractAddress, eoa, 7);
+      return await action.calculateActionParams();
+    }
+    case proposals >= 13: {
+      const action = initializeActionClass(contractAddress, eoa, 8);
+      return await action.calculateActionParams();
+    }
+  }
+};
+
 export async function strategy({
   contractAddress,
   eoa,
@@ -21,5 +81,6 @@ export async function strategy({
     },
   };
   const responseData = await subgraph.subgraphRequest(SUBGRAPH_URLS, QUERY);
-  console.log(responseData);
+
+  await calculateActionParams(responseData, contractAddress, eoa);
 }
