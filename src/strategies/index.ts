d9e9b5edcdde12dab5ca7663a09030e132e96575
Jashan Shetty <43118727+eth-jashan@users.noreply.github.com>
2023-01-23 14:54:54 +0530

    rep3 expiry strategy

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -5,11 +5,13 @@ import { StrategyType } from '../types';
 import * as rep3IsMember from './rep3-is-member';
 import * as erc20BalanceOf from './erc20-balance-of';
 import * as cultGov from './cult-strategy';
+import * as rep3BadgeExpiry from './rep3-badge-expiry';
 
 const strategies: Record<string, StrategyType> = {
   'rep3-is-member': rep3IsMember,
   'erc-20-balance-of': erc20BalanceOf,
   'cult-gov': cultGov,
+  'rep3-badge-expiry': rep3BadgeExpiry,
 };
 
 Object.keys(strategies).forEach(function(strategyName) {
