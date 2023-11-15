d62f9bb3cd608bcd141a62f12c7fe118bdf11089
Jashan Shetty <43118727+eth-jashan@users.noreply.github.com>
2023-11-15 14:57:00 +0530

    hot fix trader joe

--- src/strategies/csv-strategy/index.ts
+++ src/strategies/csv-strategy/index.ts
@@ -3,7 +3,6 @@
 import AWS from 'aws-sdk';
 import { arithmeticOperand } from '../../adapters/contract';
 import { StrategyParamsType } from '../../types';
-
 const s3 = new AWS.S3();
 //https://rep3-community-files.s3.amazonaws.com/trader_joe.csv
 async function getAndLogCsvFile(Bucket: string, bucketKey: string) {
@@ -14,7 +13,7 @@ async function getAndLogCsvFile(Bucket: string, bucketKey: string) {
   try {
     const data = await s3.getObject(params).promise();
     const csvData = data?.Body?.toString('utf-8');
-    console.log('CSV file contents:', csvData?.split('\n'));
+
     return csvData?.split('\n');
   } catch (err) {
     console.error('Error getting CSV file:', err);
@@ -26,27 +25,28 @@ const traderJoeCSVProcessing = (res: any) => {
     const singleRow = x.split(',');
     return {
       addr: singleRow[0],
-      value: parseInt(singleRow[2]),
+      value: parseFloat(singleRow[2]),
       epoches: singleRow[1],
     };
   });
   return result;
 };
+//epoch=>tie rmap
 const calculateTraderJoeEpochTier = (epoches: number) => {
-  if (epoches > 1 && epoches <= 2) {
-    return 1;
-  } else if (epoches > 3 && epoches <= 4) {
-    return 2;
-  } else if (epoches > 5 && epoches <= 6) {
-    return 3;
-  } else if (epoches > 7 && epoches <= 8) {
-    return 4;
-  } else if (epoches > 9 && epoches <= 10) {
-    return 5;
-  } else if (epoches > 11 && epoches <= 12) {
+  if (epoches >= 11) {
     return 6;
-  } else {
+  } else if (epoches >= 9) {
+    return 5;
+  } else if (epoches >= 7) {
+    return 4;
+  } else if (epoches >= 5) {
+    return 3;
+  } else if (epoches >= 3) {
+    return 2;
+  } else if (epoches >= 1) {
     return 1;
+  } else {
+    return 0;
   }
 };
 const calculateTraderJoeValueTier = (value: number) => {
@@ -70,7 +70,6 @@ const traderJoeTierCompute = (eoa: string, processedArray: any[]) => {
   const currentEoaArray = processedArray.filter(
     x => x.addr.toLowerCase() === eoa.toLowerCase()
   );
-  console.log(currentEoaArray, processedArray.length);
   if (currentEoaArray.length > 0) {
     const currentValueTier = calculateTraderJoeValueTier(
       currentEoaArray?.[0]?.value
@@ -104,16 +103,19 @@ const entagleOatCSVProcessing = (res: any) => {
   return result;
 };
 const computeDataOnType = async (
-  type: 'trader-joe' | 'entangle-oat' | any,
+  type: 'traderJoe' | 'entangleOat' | any,
   eoa: string,
   csvRes: any,
   options: any,
   tier?: number
 ) => {
   switch (type) {
-    case 'entangle-oat': {
+    case 'entangleOat': {
       const results = entagleOatCSVProcessing(csvRes);
-      return arithmeticOperand(
+      console.log(
+        results.filter(x => x.addr.toLowerCase() === eoa.toLowerCase())
+      );
+      const res = arithmeticOperand(
         parseInt(
           results.filter(x => x.addr.toLowerCase() === eoa.toLowerCase())[0]
             ?.tier
@@ -121,18 +123,18 @@ const computeDataOnType = async (
         options.threshold,
         options.operator
       );
-    }
-    case 'trader-joe': {
-      const results = traderJoeCSVProcessing(csvRes);
-      console.log(traderJoeTierCompute(eoa, results)?.tier, tier);
-      const executionResult = traderJoeTierCompute(eoa, results);
 
+      return res || false;
+    }
+    case 'traderJoe': {
+      const results = traderJoeCSVProcessing(csvRes);
+      const executionResult = traderJoeTierCompute(eoa, results);
       return tier
-        ? arithmeticOperand(executionResult?.tier, tier, '===')
+        ? arithmeticOperand(executionResult?.tier, tier, '===') || false
         : false;
     }
     default:
-      return;
+      return false;
   }
 };
 export async function strategy({ eoa, options }: StrategyParamsType) {
