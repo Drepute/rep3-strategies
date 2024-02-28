1c03991ec4cb6aacb9f98dbd353ee46ebb5276f7
Jashan Shetty <43118727+eth-jashan@users.noreply.github.com>
2024-02-28 18:32:39 +0530

    Avax Wave 1 T-2

--- src/strategies/community-strategy/v2/avax-strategy/index.ts
+++ src/strategies/community-strategy/v2/avax-strategy/index.ts
@@ -18,44 +18,64 @@ const structSwapCounts = async (userAddress: string, strategyOptions: any) => {
     { userAddress }
   );
   if (subgraphData?.depositeds.length) {
+    console.log('struct', subgraphData?.depositeds.length);
     return arithmeticOperand(
       subgraphData?.depositeds.length,
-      strategyOptions.threshold,
+      4,
       strategyOptions.operator
     )
       ? 2
+      : arithmeticOperand(
+          subgraphData?.depositeds.length,
+          2,
+          strategyOptions.operator
+        )
+      ? 1
       : 0;
   } else {
     return 0;
   }
 };
 const getWoofiEventTotalCount = async (eoa: string, strategyOptions?: any) => {
-  if (strategyOptions?.questTier === 1) {
-    const collectionName = `${strategyOptions.contractAddress}-${strategyOptions.chainId}-${strategyOptions.topic}`;
-    const filterParameter = JSON.stringify({
-      'args.from': ethers.utils.getAddress(eoa), // only from
-    });
-    const sortOptions = JSON.stringify({ blockNumber: 1 });
-    const transform_options = JSON.stringify({}); //empty obj
-    const key = 'from'; //from
-    const aggregator = 'count'; // count
-    const url = `${strategyOptions.baseUrl}/contract_service/event/aggregate?collection_name=${collectionName}&key=${key}&aggregator=${aggregator}&filter_options=${filterParameter}&sort_options=${sortOptions}&transform_options=${transform_options}`;
-    console.log(url);
-    const response = await fetch(url);
-    const res = await response.json();
-    console.log('hereee', JSON.stringify(res?.data?.result));
-    if (res?.data?.result) {
-      // return res?.data?.result;
-      return arithmeticOperand(
-        parseInt(res?.data?.result),
-        strategyOptions.threshold,
-        strategyOptions.operator
-      )
-        ? strategyOptions?.questTier
-        : 0;
-    }
-  }
+  let collectionName = `${strategyOptions.contractAddress[0]}-${strategyOptions.chainId}-${strategyOptions.topic}`;
+  let filterParameter = JSON.stringify({
+    'args.from': ethers.utils.getAddress(eoa), // only from
+  });
+  let sortOptions = JSON.stringify({ blockNumber: 1 });
+  let transform_options = JSON.stringify({}); //empty obj
+  let key = 'from'; //from
+  let aggregator = 'count'; // count
+  let url = `${strategyOptions.baseUrl}/contract_service/event/aggregate?collection_name=${collectionName}&key=${key}&aggregator=${aggregator}&filter_options=${filterParameter}&sort_options=${sortOptions}&transform_options=${transform_options}`;
+  console.log(url);
+  const response = await fetch(url);
+  const res = await response.json();
+  console.log('hereee', JSON.stringify(res?.data?.result));
+  collectionName = `${strategyOptions.contractAddress[1]}-${strategyOptions.chainId}-${strategyOptions.topic}`;
+  filterParameter = JSON.stringify({
+    'args.from': ethers.utils.getAddress(eoa), // only from
+  });
+  sortOptions = JSON.stringify({ blockNumber: 1 });
+  transform_options = JSON.stringify({}); //empty obj
+  key = 'from'; //from
+  aggregator = 'count'; // count
+  url = `${strategyOptions.baseUrl}/contract_service/event/aggregate?collection_name=${collectionName}&key=${key}&aggregator=${aggregator}&filter_options=${filterParameter}&sort_options=${sortOptions}&transform_options=${transform_options}`;
+  console.log(url);
+  const response2 = await fetch(url);
+  const res2 = await response2.json();
+  // if (res?.data?.result) {
+  // return res?.data?.result;
+  console.log('Count..........', res2?.data, res?.data);
+  const totalCount = (res2?.data?.result || 0) + (res?.data?.result || 0);
+  return arithmeticOperand(
+    parseInt(totalCount),
+    strategyOptions.threshold,
+    strategyOptions.operator
+  )
+    ? 1
+    : 0;
+  // }
 };
+
 const actionOnQuestType = async (
   type: string,
   eoa: string,
