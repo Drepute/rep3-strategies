18fe7a6d7ac6c63ee1c16e1b650dadb6b52517b3
Jashan Shetty <43118727+eth-jashan@users.noreply.github.com>
2024-03-24 13:27:39 +0530

    Chain List added

--- src/network.ts
+++ src/network.ts
@@ -42,4 +42,103 @@ export const network: NetworkObj = {
     subgraphV2:
       'https://api.thegraph.com/subgraphs/name/eth-jashan/rep3-v2-mumbai',
   },
+  '8453': {
+    name: 'Base',
+    chainId: 8453,
+    network: 'homestead',
+    rpc:
+      'https://base-mainnet.g.alchemy.com/v2/1yytYM_nUZmrCZtF41mAsF_IEZ14XlWB',
+    // ,
+    subgraph: 'https://api.thegraph.com/subgraphs/name/eth-jashan/rep3-mumbai',
+    subgraphV2:
+      'https://api.thegraph.com/subgraphs/name/eth-jashan/rep3-v2-mumbai',
+  },
+  '84532': {
+    name: 'Base Sepolia',
+    chainId: 84532,
+    network: 'homestead',
+    rpc:
+      'https://base-sepolia.g.alchemy.com/v2/z0SgXdIelcQZILk9nKzV2YBO8Cx-ySxu',
+    // ,
+    subgraph: 'https://api.thegraph.com/subgraphs/name/eth-jashan/rep3-mumbai',
+    subgraphV2:
+      'https://api.thegraph.com/subgraphs/name/eth-jashan/rep3-v2-mumbai',
+  },
+  '421614': {
+    name: 'Arbitrum Sepolia',
+    chainId: 421614,
+    network: 'homestead',
+    rpc:
+      'https://arb-sepolia.g.alchemy.com/v2/6Rj5aMHQ2P_6hA8dPx4ww7fkIz4ezwPw',
+    // ,
+    subgraph: 'https://api.thegraph.com/subgraphs/name/eth-jashan/rep3-mumbai',
+    subgraphV2:
+      'https://api.thegraph.com/subgraphs/name/eth-jashan/rep3-v2-mumbai',
+  },
+  '42161': {
+    name: 'Arbitrum',
+    chainId: 42161,
+    network: 'homestead',
+    rpc:
+      'https://arb-mainnet.g.alchemy.com/v2/951K2Ei5ZQEAwp4zIm3Y293TXVHHQ2uO',
+    // ,
+    subgraph: 'https://api.thegraph.com/subgraphs/name/eth-jashan/rep3-mumbai',
+    subgraphV2:
+      'https://api.thegraph.com/subgraphs/name/eth-jashan/rep3-v2-mumbai',
+  },
+  '11155420': {
+    name: 'OP Sepolia',
+    chainId: 11155420,
+    network: 'homestead',
+    rpc:
+      'https://opt-sepolia.g.alchemy.com/v2/K8UmbgYgJ3UxdsEzbAZBHMICiY2Eey2P',
+    // ,
+    subgraph: 'https://api.thegraph.com/subgraphs/name/eth-jashan/rep3-mumbai',
+    subgraphV2:
+      'https://api.thegraph.com/subgraphs/name/eth-jashan/rep3-v2-mumbai',
+  },
+  '10': {
+    name: 'OP',
+    chainId: 10,
+    network: 'homestead',
+    rpc:
+      'https://opt-mainnet.g.alchemy.com/v2/Q7PrLR-b0kqb70AhFIVqEtmPDaYFX-KV',
+    // ,
+    subgraph: 'https://api.thegraph.com/subgraphs/name/eth-jashan/rep3-mumbai',
+    subgraphV2:
+      'https://api.thegraph.com/subgraphs/name/eth-jashan/rep3-v2-mumbai',
+  },
+  '11155111': {
+    name: 'Sepolia',
+    chainId: 11155111,
+    network: 'homestead',
+    rpc:
+      'https://eth-sepolia.g.alchemy.com/v2/FJG5-gtZMg3gEM_s53uIhiiXpqY5-mXF',
+    // ,
+    subgraph: 'https://api.thegraph.com/subgraphs/name/eth-jashan/rep3-mumbai',
+    subgraphV2:
+      'https://api.thegraph.com/subgraphs/name/eth-jashan/rep3-v2-mumbai',
+  },
+  '1442': {
+    name: 'Polygon zkEVM Testnet',
+    chainId: 1442,
+    network: 'homestead',
+    rpc:
+      'https://polygonzkevm-testnet.g.alchemy.com/v2/6a02FldHuZy8ttIe9XKVWGF22__P8GC5',
+    // ,
+    subgraph: 'https://api.thegraph.com/subgraphs/name/eth-jashan/rep3-mumbai',
+    subgraphV2:
+      'https://api.thegraph.com/subgraphs/name/eth-jashan/rep3-v2-mumbai',
+  },
+  '1101': {
+    name: 'Polygon zkEvm',
+    chainId: 1101,
+    network: 'homestead',
+    rpc:
+      'https://polygonzkevm-mainnet.g.alchemy.com/v2/CKAFo_PRO7Ae0jbrayQRdF9-6r4JEXIG',
+    // ,
+    subgraph: 'https://api.thegraph.com/subgraphs/name/eth-jashan/rep3-mumbai',
+    subgraphV2:
+      'https://api.thegraph.com/subgraphs/name/eth-jashan/rep3-v2-mumbai',
+  },
 };
